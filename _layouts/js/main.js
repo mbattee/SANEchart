2ddd6bdfcb9fb3a0fc10a9c8fc41d1f6ac342f07
mbattee <tinaswinkels@Tinas-MacBook-Air.local>
2017-03-12 10:20:50 +0800

    final before complete style

---
+++ _layouts/js/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  console.log('hello');
+
+  $(".faq-question").click(function(){
+    $(this).children(".faq-answer").slideToggle();
+  });
+
+  $(".invite-button").click(function(){
+    $(".invite-popup").slideToggle();
+  });
+  $('#portfolio-grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200,
+  gutter: 10,
+  });
+
+
+
+});
